597a092e3d8e0068708ef35a40cef5c4fd39f539
Serhii Klymets <ksd2000@ukr.net>
2023-05-17 22:58:55 +0300

    дз

--- src/components/App.js
+++ src/components/App.js
@@ -1,78 +1,71 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { ContactForm } from './contactForm/ContactForm';
 import { Filter } from './filter/Filter';
 import { ContactList } from './contactList/ContactList';
 import { nanoid } from "nanoid";
 import css from './App.module.css'
 
-export class App extends Component {
+export const App = () => {
 
-  state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: '',
-  };
+  const exampleBook =
+    [ { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
+    ];
 
-  addContact = (userData) => {
-      const newUser = { ...userData, id: nanoid() };
-      this.setState(
-        (prevstate) => {
-          return { contacts: [...prevstate.contacts, newUser] }
-        }
+  const [contacts, setContacts] = useState(() => {
+    const book = localStorage.getItem('phonebook');
+    if (book) {
+      const parsePhonebook = JSON.parse(book);
+      return parsePhonebook
+    } else {
+      return exampleBook
+    }
+  });  
+
+  const [filter, setFilter] = useState('')
+
+  const addContact = (userData) => {
+    const newUser = { ...userData, id: nanoid() };
+    setContacts(
+      prevstate => [...prevstate, newUser]
       )
   };
-  
-  deleteContacts = (id) => {
-    this.setState((prevstate) => {
-      return { contacts: prevstate.contacts.filter((user) => user.id !== id) }
+  //
+  const deleteContacts = (id) => {
+    setContacts(prevstate => { 
+      const newState = prevstate.filter((user) => user.id !== id)
+      return newState
     })
   };
-  
-  handleChangeFilter = ({ target: { value } }) => {
-    this.setState({
-      filter: value
-    })
+                                              //запись в state--filter значения input (event.target.value)
+  const handleChangeFilter = ({ target: { value } }) => {
+    setFilter(value)
   };
 
-  filterContact = (name, filter) => {
+  const filterContact = (name, filter) => {
     let nameLow = name.toLocaleLowerCase();
     let filterLow = filter.toLocaleLowerCase();
     return (nameLow.indexOf(filterLow) >= 0)
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('phonebook', JSON.stringify(this.state.contacts))
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('phonebook', JSON.stringify(contacts))
+  }, [contacts]);
 
-  componentDidMount() {
-    const book = localStorage.getItem('phonebook');
-    if (book) {
-      const parsePhonebook = JSON.parse(book);
-      this.setState({contacts: parsePhonebook})
-    }
-  }
-  
-  render() {
-
-    const { contacts, filter } = this.state;
-    const contactSeach = contacts.filter((user) => this.filterContact(user.name, filter));
+    const contactSeach = contacts.filter((user) => filterContact(user.name, filter));
     const namesContact = contacts.map(user => user.name);
 
     return (
       <div className={css.phonebook}>
         <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} namesContact={namesContact} />
+        <ContactForm addContact={addContact} namesContact={namesContact} />
 
         <h2>Contacts</h2>
-        <Filter handleChangeFilter={this.handleChangeFilter} filter={filter} />
-        <ContactList contactSeach={contactSeach} deleteContacts={this.deleteContacts} />
+        <Filter handleChangeFilter={handleChangeFilter} filter={filter} />
+        <ContactList contactSeach={contactSeach} deleteContacts={deleteContacts} />
       </div>
     );
-  }
+
 }
