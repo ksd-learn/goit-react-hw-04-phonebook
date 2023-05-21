35b5e8818a1bd4031e76974ab40ade67ef6ca7a0
Serhii Klymets <ksd2000@ukr.net>
2023-05-21 14:58:26 +0300

    дз-ver2

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,10 @@ import { ContactForm } from './contactForm/ContactForm';
 import { Filter } from './filter/Filter';
 import { ContactList } from './contactList/ContactList';
 import { nanoid } from "nanoid";
-import css from './App.module.css'
+import css from './App.module.css';
+import { exampleBook } from '../data/exampleBook';
 
 export const App = () => {
-
-  const exampleBook =
-    [ { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
-    ];
-
   const [contacts, setContacts] = useState(() => {
     const book = localStorage.getItem('phonebook');
     if (book) {
@@ -26,13 +19,20 @@ export const App = () => {
 
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    localStorage.setItem('phonebook', JSON.stringify(contacts))
+  }, [contacts]);  
+
   const addContact = (userData) => {
-    const newUser = { ...userData, id: nanoid() };
-    setContacts(
-      prevstate => [...prevstate, newUser]
-      )
+    let isExist = contacts.find((item) => item.name === userData.name)
+    if (isExist) {
+        alert(`${userData.name} is already in contacts`);
+    } else {
+      const newUser = { ...userData, id: nanoid() };
+      setContacts(prevstate => [...prevstate, newUser])
+    }
   };
-  //
+
   const deleteContacts = (id) => {
     setContacts(prevstate => { 
       const newState = prevstate.filter((user) => user.id !== id)
@@ -50,22 +50,17 @@ export const App = () => {
     return (nameLow.indexOf(filterLow) >= 0)
   };
 
-  useEffect(() => {
-    localStorage.setItem('phonebook', JSON.stringify(contacts))
-  }, [contacts]);
+  const contactSeach = contacts.filter((user) => filterContact(user.name, filter));
 
-    const contactSeach = contacts.filter((user) => filterContact(user.name, filter));
-    const namesContact = contacts.map(user => user.name);
-
-    return (
-      <div className={css.phonebook}>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={addContact} namesContact={namesContact} />
-
-        <h2>Contacts</h2>
-        <Filter handleChangeFilter={handleChangeFilter} filter={filter} />
-        <ContactList contactSeach={contactSeach} deleteContacts={deleteContacts} />
-      </div>
-    );
+  return (
+    <div className={css.phonebook}>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      
+      <h2>Contacts</h2>
+      <Filter handleChangeFilter={handleChangeFilter} filter={filter} />
+      <ContactList contactSeach={contactSeach} deleteContacts={deleteContacts} />
+    </div>
+  );
 
 }
